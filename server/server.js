9276cde0cf820d73c44eda58e33adb9a2431c9bd
ALBERT <albertgbeaca@icloud.com>
2026-02-02 15:20:25 +0000

    boilerplate testing

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import pg from "pg"
+    
+const app = express()
+app.use(express.json())
+app.use(cors())
+dotenv.config()
+
+    
+const db = new pg.Pool({
+    connectionString: process.env.DB_CONN ///////////////
+})
